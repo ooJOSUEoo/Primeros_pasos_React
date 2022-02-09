b8b0a54f391f0f09f3133070857f73041484e882
Josue <sjosue5082002@gmail.com>
2022-02-09 10:42:27 -0600

    Nuestro primer Componente

---
+++ src/PrimeraApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+//FC
+
+const PrimeraApp = () => {
+    return <h1>Hola Mundo</h1>
+}
+
+export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PrimeraApp from "./PrimeraApp";
+import "./index.css";
 
 const saludo = <h1>Hola Mundo</h1>;
 
 const divRoot = document.querySelector("#app");
 
-ReactDOM.render(saludo, divRoot);
+ReactDOM.render(<PrimeraApp />, divRoot);
 
